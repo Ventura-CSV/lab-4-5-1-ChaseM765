f8ec812fa4b844b384f4b5c3af9af99fa5d9aea5
ChaseM765 <ChaseM765>
2024-06-28 00:28:20 -0700

    CODE ADDED

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import random
 
 
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    import random
+    x = 0
+    total = 0
+    numbers = []
+    for x in range (0,5):
+        ran = random.randint(0,100)
+        numbers.append(ran)
+        total += ran
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
